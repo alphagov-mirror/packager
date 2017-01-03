e06a7c0c5f2eaebc2ee814404f38e846abab3701
Laura Martin <surminus@gmail.com>
2017-01-03 15:23:56 +0000

    Add dependencies for hiera-eyaml gem
    The hiera-eyaml gem added in 109249f092cae7402d71238aca2662668eac9ecf requires
    both trollop and highline, which need to also be installed as .deb
    packages.

---
+++ fpm/recipes/rubygem-highline/recipe.rb
@@ -0,0 +1,4 @@
+class RubyHighline < FPM::Cookery::RubyGemRecipe
+  name    "highline"
+  version "1.6.21"
+end
---
+++ fpm/recipes/rubygem-trollop/recipe.rb
@@ -0,0 +1,4 @@
+class RubyTrollop < FPM::Cookery::RubyGemRecipe
+  name    "trollop"
+  version "2.1.2"
+end
